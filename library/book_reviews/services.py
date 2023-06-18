0b4c4f9fbd4fc324855d60b0fbbfe87619207d4c
franz-naling <fnal136@aucklanduni.ac.nz>
2023-06-19 07:32:33 +1200

    Deleted exceptions + new exception, deleted unnecessary code

--- library/book_reviews/services.py
+++ library/book_reviews/services.py
@@ -1,25 +1,16 @@
 from typing import List, Iterable
 
 from library.adapters.repository import AbstractRepository
+from library.books.services import NonExistentBookException
 from library.domain.model import Book, Review, Shelf, Author
 
 
-class NonExistentBookException(Exception):
+class CharacterLimitException(Exception):
     pass
 
 
-class UnknownUserException(Exception):
-    pass
-
-
-def add_comment(book_id: int, user_name: str, review_text: str, rating: int, repo: AbstractRepository):
+def add_review(book_id: int, user_name: str, review_text: str, rating: int, repo: AbstractRepository):
     book = repo.get_book_by_id(book_id)
-    if book is None:
-        raise NonExistentBookException
-
-    user = repo.get_user(user_name)
-    if user is None:
-        raise UnknownUserException
 
     repo.add_book_review(
         Review(
